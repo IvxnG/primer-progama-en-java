933553a926deb9193258983083f83deae4b8b6ff
Ivxn <gonzalez.martino.ivan@gmail.com>
2021-10-20 10:33:46 +0200

    sin nombre + DJ favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class MiPrimerClaseVersion3 {
 	
 	public static void main(String args[]) {
 		
-		System.out.println("Ivan Gonzalez|");
 		System.out.println("Leon, 2002|");
-		System.out.println("Valencia de don juan Windows 10");
+		System.out.println("Valencia de don juan Windows 10");  
+		System.out.println("Avicii");
 		
 	}
 }
